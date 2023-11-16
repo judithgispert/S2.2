e0de9a6d0f2215cec274e617600aea2df55b5020
Judith Gispert <146069863+judithgispert@users.noreply.github.com>
2023-11-16 08:56:19 +0100

    Add files via upload

---
+++ Shop/Queries - Shop.sql
@@ -0,0 +1,129 @@
+USE tienda;
+
+-- 1. List the names of all products in the PRODUCTO table:
+SELECT nombre AS Product_name FROM producto;
+
+-- 2. List the names + prices of all products in the PRODUCTO table:
+SELECT nombre AS Product_name, precio AS Product_pice FROM producto;
+
+-- 3. List all the columns of the PRODUCTO table:
+SELECT * FROM producto;
+
+-- 4. List the names + prices in € + prices in $ off all the products in PRODUCTO table:
+SELECT nombre AS Product_name, precio AS Price_€, ROUND(precio * 1.07, 2) AS Price_$ FROM producto;
+
+-- 5. List the names + prices in € + prices in $ off all the products in PRODUCTO table, use these names for the columns (Product name, euros, dollars):
+SELECT nombre AS 'Product name', precio AS 'Euros', ROUND(precio * 1.07, 2) AS 'Dollars' FROM producto;
+
+-- 6. List the names + prices of all the products in the PRODUCTO table, changing names to uppercase:
+SELECT UPPER(nombre) AS Product_name, precio AS Price FROM producto;
+
+-- 7. List the names + prices of all the products in the PRODUCTO table, changing names to lowercase:
+SELECT LOWER(nombre) AS Product_name, precio AS Price FROM producto;
+
+-- 8. List the name of all manufacturers in one column, in the other, get the first 2 char (uppercase) of the name:
+SELECT nombre, SUBSTRING(UPPER(nombre), 1, 2) AS nombre_2 FROM fabricante;
+
+-- 9. List the names + prices (rounded result) of all products in the PRODUCTO table:
+SELECT nombre AS Product_name, ROUND(precio) AS Rounded_price FROM producto;
+
+-- 10. List the names + prices (truncate price value to display without decimals) of all products in the PRODUCTO table:
+SELECT nombre AS Product_name, TRUNCATE (precio, 0) AS Price_without_Decimals FROM producto;
+
+-- 11. List the code of the manufacturers that have products in the PRODUCTO table:
+SELECT f.codigo AS Manufacturer_code FROM fabricante f INNER JOIN producto p ON codigo_fabricante = f.codigo;
+
+-- 12. List the code of the manufacturers that have products in the PRODUCTO table (delete repeated codes):
+SELECT DISTINCT f.codigo AS Manufacturer_code FROM fabricante f INNER JOIN producto p ON codigo_fabricante = f.codigo;
+
+-- 13. List the names of all the manufacturers in ascending order:
+SELECT nombre AS Manufacturer_name_ASC FROM fabricante ORDER BY nombre ASC;
+
+-- 14. List the names of all the manufacturers in descending order:
+SELECT nombre AS Manufacturer_name_DESC FROM fabricante ORDER BY nombre DESC;
+
+-- 15. List the names of all the products, first ascending order, then the price in descending order:
+SELECT nombre AS Product_name, precio AS Price FROM producto ORDER BY nombre ASC, precio DESC;
+
+-- 16. Returns a list with the first 5 rows of the FABRICANTE table:
+SELECT * FROM fabricante LIMIT 5;
+
+-- 17. Returns a list with 2 row starting from the 4th row (included) of FABRICANTE table:
+SELECT * FROM fabricante LIMIT 2 OFFSET 3;
+
+-- 18. List the cheapest product (name + price), use Order by and Limit clauses:
+SELECT nombre AS Product_name, precio AS Price FROM producto ORDER BY precio ASC LIMIT 1;
+
+-- 19. List the most expensive product (name + price), use ORDER BY and LIMIT clauses:
+SELECT nombre AS Product_name, precio AS Price FROM producto ORDER BY precio DESC LIMIT 1;
+
+-- 20. List the name of all the products where manufacturer code = 2:
+SELECT nombre AS Product_name FROM producto WHERE codigo_fabricante = 2;
+
+-- 21. Return a list with (product name + product price + manufacturer name) of all products in the database:
+SELECT p.nombre AS Product_name, precio AS Product_price, f.nombre AS Manufacturer_name FROM producto p INNER JOIN fabricante f ON  codigo_fabricante = f.codigo;
+
+-- 22. Return a list with (product name + product price + manufacturer name) of all products in the database. Sort the result by manufacturer name (alphabetical order):
+SELECT p.nombre AS Product_name, precio AS Price, f.nombre AS Manufacturer_name FROM producto p INNER JOIN fabricante f ON  codigo_fabricante = f.codigo ORDER BY f.nombre ASC;
+
+-- 23. Return a list with (product code + product name + manufacturer code + manufacturer name) of all the products in the database:
+SELECT p.codigo AS Product_code, p.nombre AS Product_name, f.codigo AS Manufacturer_code, f.nombre AS Manufacturer_name FROM producto p INNER JOIN fabricante f ON codigo_fabricante = f.codigo;
+
+-- 24. Return (product name + product price + manufacturer name) of the cheapest product:
+SELECT p.nombre AS Product_name, p.precio AS Product_price, f.nombre AS Manufacturer_name FROM producto p INNER JOIN fabricante f ON codigo_fabricante = f.codigo ORDER BY p.precio ASC LIMIT 1;
+
+-- 25. Return (product name + product price + manufacturer name) of the most expensive product:
+SELECT p.nombre AS Product_name, p.precio AS Product_price, f.nombre AS Manufacturer_name FROM producto p INNER JOIN fabricante f ON codigo_fabricante = f.codigo ORDER BY p.precio DESC LIMIT 1;
+
+-- 26. Returns a list of all products from manufacturer LENOVO:
+SELECT * FROM producto p INNER JOIN fabricante f ON codigo_fabricante = f.codigo WHERE f.nombre = 'Lenovo';
+
+-- 27. Returns a list of all products from manufacturer CRUCIAL that (price < 200€):
+SELECT * FROM producto p INNER JOIN fabricante f ON codigo_fabricante = f.codigo WHERE f.nombre = 'Crucial' && p.precio > 200;
+
+-- 28. Resturns a list with all products from manufacturers (Asus, Hewlett-Packard & Seagate), without using the IN operator:
+SELECT * FROM producto p INNER JOIN fabricante f ON codigo_fabricante = f.codigo WHERE f.nombre = 'Asus' OR f.nombre = 'Hewlett-Packard' OR f.nombre = 'Seagate';
+
+-- 29. Resturns a list with all products from manufacturers (Asus, Hewlett-Packard & Seagate), using the IN operator:
+SELECT * FROM producto INNER JOIN fabricante ON codigo_fabricante = fabricante.codigo WHERE fabricante.nombre IN ('Asus', 'Hewlett-Packard', 'Seagate');
+
+-- 30. Returns a list with name + price of all products  where manufacturers name ends with 'e':
+SELECT p.nombre AS Product_name, p.precio AS Product_price FROM producto p INNER JOIN fabricante f ON codigo_fabricante = f.codigo WHERE f.nombre LIKE '%e';
+
+-- 31. Returns a list with name + price of all products where manufacturers name contains 'w':
+SELECT p.nombre AS Product_name, p.precio AS Product_price FROM producto p INNER JOIN fabricante f ON codigo_fabricante = f.codigo WHERE f.nombre LIKE '%w%';
+
+-- 32. Returns a list with (product name + product price + manufacturer name) of all products that price >= 180€. First, sort the result by price (DESC) and second by name (ASC):
+SELECT p.nombre AS Product_name, p.precio AS Product_price, f.nombre AS Manufacturer_name FROM producto p INNER JOIN fabricante f ON codigo_fabricante = f.codigo WHERE p.precio >= 180 ORDER BY p.precio DESC, p.nombre ASC;
+
+-- 33. Returns a list with manufacturer code + name, only of those manufacturers that have products in the database:
+SELECT DISTINCT f.codigo AS Manufacturer_code, f.nombre AS Manufacturer_name FROM fabricante f INNER JOIN producto p ON f.codigo = codigo_fabricante;
+
+-- 34. Returns a list of all the manufacturers that exist + products. The list must also show those manufacturers that don't have products:
+SELECT * FROM fabricante f LEFT OUTER JOIN producto p ON f.codigo = codigo_fabricante;
+
+-- 35. Returns a list showing only those manufacturers that don't have products:
+SELECT * FROM fabricante f LEFT OUTER JOIN producto p ON f.codigo = codigo_fabricante WHERE codigo_fabricante IS NULL;
+
+-- 36. Returns all products from the manufacturer LENOVO (without using INNER JOIN):
+SELECT * FROM producto p LEFT OUTER JOIN fabricante f ON codigo_fabricante = f.codigo WHERE f.nombre = 'Lenovo';
+
+-- 37. Returns all products that have the same price as the most expensive product from manufacturer LENOVO (without using INNER JOIN):
+SELECT * FROM producto p LEFT OUTER JOIN fabricante f ON codigo_fabricante = f.codigo WHERE p.precio = (SELECT MAX(p.precio) FROM producto p LEFT OUTER JOIN fabricante f ON codigo_fabricante = f.codigo WHERE f.nombre = 'Lenovo');
+
+-- 38. List the name of the most expensive product from the manufacturer LENOVO:
+SELECT p.nombre AS Product_name FROM producto p INNER JOIN fabricante f ON codigo_fabricante = f.codigo WHERE p.precio = (SELECT MAX(p.precio) FROM producto p INNER JOIN fabricante f ON codigo_fabricante = f.codigo WHERE f.nombre = 'Lenovo');
+
+-- 39. List the cheapest product name from the manufacturer HEWLETT-PACKARD:
+SELECT p.nombre AS Product_name FROM producto p INNER JOIN fabricante f ON codigo_fabricante = f.codigo WHERE f.nombre = 'Hewlett-Packard' ORDER BY p.precio ASC LIMIT 1;
+
+-- 40. Returns all products in the database that have a price >= the most expensive product from manufacturer LENOVO:
+SELECT p.nombre AS Product_name FROM producto p INNER JOIN fabricante f ON codigo_fabricante = f.codigo WHERE p.precio >= (SELECT MAX(p.precio) FROM producto p INNER JOIN fabricante f ON codigo_fabricante = f.codigo WHERE f.nombre = 'Lenovo');
+
+-- 41. List all products from manufacturer ASUS that have price > the average price of all their products:
+SELECT * FROM producto p INNER JOIN fabricante f ON codigo_fabricante = f.codigo WHERE p.precio > (SELECT AVG(p.precio) FROM producto p INNER JOIN fabricante f ON codigo_fabricante = f.codigo WHERE f.nombre = 'Asus') AND f.nombre = 'Asus';
+
+
+
+
+
---
+++ University/Queries - University.sql
@@ -0,0 +1,81 @@
+USE Universidad;
+
+-- 1. Returns a list with (surname 1 + surname 2 + name) of all the students. The list must be ordered alphabetically from lowest to highest by surname 1, surname 2 and name.
+SELECT apellido1 AS 'Surname 1', apellido2 AS 'Surname 2', nombre AS 'Student name' FROM persona ORDER BY apellido1, apellido2, nombre;
+
+-- 2. Fins the first and last names of students who haven't registered their phone number in the database.
+SELECT nombre AS 'Name', apellido1 AS 'Surname 1', apellido2 AS 'Surname 2' FROM persona WHERE telefono IS NULL;
+
+-- 3. Returns the list of students who were born in 1999.
+SELECT * FROM persona WHERE YEAR(fecha_nacimiento) = 1999 AND tipo = 'alumno';
+
+-- 4. Returns the list of teachers who haven't registered their phone number in the database and algo their nif ends in K.
+SELECT * FROM persona WHERE tipo = 'profesor' AND telefono IS NULL AND nif LIKE '%K';
+
+-- 5. Returns the list of subjects that are in the first semester, in the third year of the degree that has the id = 7.
+SELECT * FROM asignatura WHERE cuatrimestre = 1 AND curso = 3 AND id_grado = 7;
+
+-- 6. Returns a list of teachers + their departament name. Show four columns (surname 1, surname 2, name and department name). The result will be sorted alphabetically from lowest to highest by surname and name.
+SELECT p.apellido1 AS 'Surname 1', p.apellido2 AS 'Surname 2', p.nombre AS 'Teacher name', pr.id_departamento AS 'Department name' FROM persona p INNER JOIN departamento d INNER JOIN profesor pr ON d.id = pr.id_departamento AND p.id = pr.id_profesor ORDER BY p.apellido1 DESC, p.apellido2 DESC, p.nombre DESC;
+
+-- 7. Returns a list of (the names of the subjects + start university year + end university year) of the student's with NIF 26902806M.
+SELECT a.nombre AS 'Subject name', c.anyo_inicio AS 'Year (start)', c.anyo_fin AS 'Year (End)' FROM curso_escolar c INNER JOIN alumno_se_matricula_asignatura am INNER JOIN persona p INNER JOIN asignatura a ON am.id_alumno = p.id AND am.id_curso_escolar = c.id AND am.id_asignatura = a.id WHERE p.nif = '26902806M';
+
+-- 8. Returns a list of the names of all the departments that have teachers who teach a subject in the "Grau en Enginyeria Informàtica (Pla 2015)".
+SELECT DISTINCT d.nombre AS 'Department name' FROM departamento d INNER JOIN profesor p INNER JOIN asignatura a INNER JOIN grado g ON d.id = p.id_departamento AND a.id_profesor = p.id_profesor AND a.id_grado = g.id WHERE g.nombre = 'Grado en Ingeniería Informática (Plan 2015)';
+
+-- 9. Returns a list of all students who have enrolled in a subject during the 2018/2019 university year.
+SELECT DISTINCT p.* FROM persona p INNER JOIN curso_escolar c INNER JOIN alumno_se_matricula_asignatura am ON p.id = am.id_alumno AND c.id = am.id_curso_escolar WHERE c.anyo_inicio = 2018 AND c.anyo_fin = 2019;
+
+/* QUERIES WITH LEFT JOIN AND RIGHT JOIN */
+-- 1. Returns a list of the names of all the professors and departments they are linked to. The list must also show those teachers who don't have any associate department. The list must return four columns, name of the department, surname 1, surname 2 and name of the teacher. The result will be ordered alphabetically from lowest to highest by department name, surnames and name.
+SELECT d.nombre AS 'Department name', p.apellido1 AS 'Surname 1', p.apellido2 AS 'Surname 2', p.nombre AS 'Name' FROM persona p JOIN departamento d RIGHT OUTER JOIN profesor pr ON p.id = pr.id_profesor AND d.id = pr.id_departamento ORDER BY d.nombre DESC, p.apellido1 DESC, p.apellido2 DESC, p.nombre DESC;
+
+-- 2. Returns a list of professors who aren't associated with a department.
+SELECT p.* FROM persona p LEFT OUTER JOIN profesor pr ON p.id = pr.id_profesor WHERE tipo = 'profesor' AND pr.id_departamento IS NULL;
+
+-- 3. Returns a list of departments that don't have associate professors.
+SELECT d.* FROM departamento d LEFT OUTER JOIN profesor pr ON d.id = pr.id_departamento WHERE id_departamento IS NULL;
+
+-- 4. Returns a list of teachers who don't teach any subjects.
+SELECT p.* FROM persona p LEFT OUTER JOIN profesor pr ON p.id = pr.id_profesor LEFT OUTER JOIN asignatura a ON pr.id_profesor = a.id_profesor WHERE p.tipo = 'profesor' AND a.nombre IS NULL;  
+
+-- 5. Returns a list of subjects that don't have an assigned teacher.
+SELECT a.* FROM asignatura a LEFT OUTER JOIN profesor pr ON a.id_profesor = pr.id_profesor WHERE a.id_profesor IS NULL; 
+
+-- 6. Returns a list of all departments that haven't taught subjects in any university year.
+SELECT DISTINCT d.* FROM departamento d LEFT OUTER JOIN profesor pr ON d.id = pr.id_departamento LEFT OUTER JOIN asignatura a ON pr.id_profesor = a.id_profesor WHERE a.curso IS NULL; 
+
+/* SUMMARY QUERIES*/
+-- 1. Returns the total number of students there.
+SELECT COUNT(*) AS 'Total num of students' FROM persona WHERE tipo = 'alumno';
+
+-- 2. Calculate how many students were born in 1999.
+SELECT COUNT(*) AS 'Students who were born in 1999' FROM persona WHERE tipo = 'alumno' AND YEAR(fecha_nacimiento) = 1999;
+
+-- 3. Calculate how many teachers there are in each department. The result should only show two columns, one with the name of the department and another with the number of teachers in that department. The result must only include the departments that have associate professors and must be ordered from highest to lowest by the number of professors.
+SELECT d.nombre AS 'Department name', COUNT(pr.id_profesor) AS 'Number of teachers' FROM departamento d INNER JOIN profesor pr ON d.id = pr.id_departamento GROUP BY d.nombre ORDER BY COUNT(pr.id_profesor) DESC;
+
+-- 4. Returns a list with all the departments and the number of professors in each of them. Keep in mind that there may be departments that don't have associate professors. These departments must also appear in the list.
+SELECT d.nombre AS 'Department name', COUNT(pr.id_profesor) AS 'Number of teachers' FROM departamento d LEFT OUTER JOIN profesor pr ON d.id = pr.id_departamento GROUP BY d.nombre;
+
+-- 5. Returns a list with the name of all the existing degrees in the database and the number of subjects each one has. There may be degrees that don't have associated subjects. These grades must also appear in the listing. The result must be ordered from highest to lowest by the number of subjects.
+SELECT DISTINCT g.nombre AS 'Grade name', COUNT(a.nombre) AS "Number of subjects" FROM grado g LEFT OUTER JOIN asignatura a ON g.id = a.id_grado GROUP BY g.nombre ORDER BY COUNT(a.nombre) DESC;
+
+-- 6. Returns a list with the name of all the existing degrees in the database and the number of subjects each has, of the degrees that have more than 40 associated subjects.
+SELECT DISTINCT g.nombre AS 'Grade name', COUNT(a.nombre) AS "Number of subjects" FROM grado g INNER JOIN asignatura a ON g.id = a.id_grado GROUP BY g.nombre HAVING COUNT(a.nombre) > 40;
+
+-- 7. Returns a list showing the name of degrees and the total number of credits for each subject type. The result must have three columns: name of the degree, type of subject and the sum of the credits of all subjects of that type.
+SELECT g.nombre AS 'Grade name', a.tipo AS "Subject type", SUM(a.creditos) AS 'Number of credits' FROM grado g INNER JOIN asignatura a ON g.id = a.id_grado GROUP BY a.tipo, g.nombre; 
+
+-- 8. Returns a list that shows how many students have enrolled in a subject in each of the school years. The result must show two columns, one column with the start year of the school year and another with the number of enrolled students.
+SELECT c.anyo_inicio AS "Course start year", COUNT(am.id_alumno) AS "Number of enrolled students" FROM curso_escolar c LEFT OUTER JOIN alumno_se_matricula_asignatura am ON am.id_curso_escolar = c.id LEFT OUTER JOIN asignatura a ON a.id = am.id_asignatura GROUP BY c.anyo_inicio; 
+
+-- 9. Returns a list with the number of subjects taught by each teacher. The list must take into account those professors who do not teach any subjects. The result will show five columns: id, name, first last name, second last name and number of subjects. The result will be ordered from highest to lowest by the number of subjects.
+SELECT p.id AS 'ID teacher', p.nombre AS 'Name', p.apellido1 AS 'Surname 1', p.apellido2 AS 'Surname 2', COUNT(a.nombre) AS "Number of subjects" FROM persona p LEFT OUTER JOIN profesor pr ON p.id = pr.id_profesor LEFT OUTER JOIN asignatura a ON a.id_profesor = pr.id_profesor WHERE p.tipo = 'profesor' GROUP BY p.id ORDER BY COUNT(a.nombre) DESC; 
+
+-- 10. Returns all data for the youngest student.
+SELECT * FROM persona p WHERE p.tipo = 'alumno' AND p.fecha_nacimiento = (SELECT MAX(p.fecha_nacimiento) FROM persona p);
+
+-- 11. Returns a list of professors who have an associated department and who do not teach any subjects.
+SELECT pr.* FROM profesor pr LEFT OUTER JOIN asignatura a ON pr.id_profesor = a.id_profesor WHERE pr.id_departamento IS NOT NULL AND a.id_profesor IS NULL;
